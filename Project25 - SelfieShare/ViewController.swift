686c90f997d1c6d1de995841197ff3a20638c63b
Noah Pope <npope@tutanota.com>
2024-12-03 10:14:44 -0600

    Initial Commit

---
+++ Project25 - SelfieShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25 - SelfieShare
+//
+//  Created by Noah Pope on 12/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
